c1dea8c4a34e5a1a8d3f911e696f52713d9472a7
m52v <morem0t.ev@gmail.com>
2025-05-22 01:46:45 +0700

    KS-1-3 gagarin

---
+++ src/main/kotlin/lesson_1/ex_3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main () {
+
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("Год полёта: $year")
+    println("Время взлёта (часы): $hour")
+    println("Время взлёта (минуты): $minute")
+
+    hour = 10
+    minute = 55
+    println("Время посадки: $hour:$minute")
+
+}
