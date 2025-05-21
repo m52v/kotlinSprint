c89ff5a75cef8dd3bc3fa2db84efa9d3193e5605
m52v <morem0t.ev@gmail.com>
2025-05-21 22:30:40 +0700

    KS-1-1 orders and message

---
+++ src/main/kotlin/lesson_1/ex_1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_1
+
+fun main () {
+
+    val totalOrders = 75
+    val messageAfterOrder = "Спасибо за заказ! Приходите еще:)"
+    println("Количество заказов: $totalOrders")
+    println(messageAfterOrder)
+
+}
