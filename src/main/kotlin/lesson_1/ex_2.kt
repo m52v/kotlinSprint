42e798f1a58e08ab8379719aca51afd3841b9b17
m52v <morem0t.ev@gmail.com>
2025-05-22 01:25:32 +0700

    KS-1-2 employees count

---
+++ src/main/kotlin/lesson_1/ex_2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main () {
+
+    val totalOrders = 75
+    val messageAfterOrder = "Спасибо за заказ! Приходите еще:)"
+    println("Количество заказов: $totalOrders")
+    println(messageAfterOrder)
+
+    var countEmployees = 2000
+    println("Количество работников: $countEmployees")
+
+    countEmployees = 1999
+    println("Актуальное кол-во работников: $countEmployees")
+
+}
