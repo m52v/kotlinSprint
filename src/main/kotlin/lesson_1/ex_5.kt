719dfb32a9f014610f9f5d3496f6f0780dd217bd
m52v <morem0t.ev@gmail.com>
2025-05-23 00:29:14 +0700

    KS-1-5 convert seconds

--- src/main/kotlin/lesson_1/ex_5.kt
+++ src/main/kotlin/lesson_1/ex_5.kt
@@ -1,16 +1,18 @@
 package org.example.lesson_1
 
-fun main () {
+const val SECONDS_IN_MINUTE = 60
+const val MINUTES_IN_HOUR = 60
+fun main() {
     val totalSeconds = 6480
 
-    val hours = totalSeconds / 3600
-    val secondsAfterHours = totalSeconds - (hours * 3600)
+    val hours = totalSeconds / (SECONDS_IN_MINUTE * MINUTES_IN_HOUR)
+    val secondsAfterHours = totalSeconds - (hours * SECONDS_IN_MINUTE * MINUTES_IN_HOUR)
 
-    val minutes = secondsAfterHours / 60
-    val secondsAfterMinutes = totalSeconds - (hours * 3600 + minutes * 60)
+    val minutes = secondsAfterHours / SECONDS_IN_MINUTE
+    val secondsAfterMinutes = secondsAfterHours - (minutes * SECONDS_IN_MINUTE)
 
     val seconds = secondsAfterMinutes
 
-    println("0$hours:$minutes:0$seconds")
-
+    val result = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    println(result)
 }
