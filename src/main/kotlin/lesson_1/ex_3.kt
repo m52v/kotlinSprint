c558f54bef89c03ab841604d0ef3379bf7e61c90
m52v <morem0t.ev@gmail.com>
2025-05-22 23:25:56 +0700

    KS-1-3 gagarin

--- src/main/kotlin/lesson_1/ex_3.kt
+++ src/main/kotlin/lesson_1/ex_3.kt
@@ -1,16 +1,16 @@
 package org.example.lesson_1
 
-fun main () {
+fun main() {
 
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
     println("Год полёта: $year")
     println("Время взлёта (часы): $hour")
     println("Время взлёта (минуты): $minute")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     print("Время посадки: ")
     println("$hour:$minute")
 
