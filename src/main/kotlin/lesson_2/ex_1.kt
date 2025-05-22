1b726cfce82cfc5e787da8a26700c52ec1f63081
m52v <morem0t.ev@gmail.com>
2025-05-22 21:02:16 +0700

    KS-2-1 average grade in class

---
+++ src/main/kotlin/lesson_2/ex_1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main () {
+
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    val averageGradeEnglish = (student1 + student2 + student3 + student4) / 4.0
+
+    println("Средний балл по английскому в классе: $averageGradeEnglish")
+
+}
