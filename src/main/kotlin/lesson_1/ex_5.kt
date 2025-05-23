7fbe0e221cc81a94b4c25f236c4bcc7dd96b1b1e
m52v <morem0t.ev@gmail.com>
2025-05-24 04:33:37 +0700

    KS-1-5 convert seconds

--- src/main/kotlin/lesson_1/ex_5.kt
+++ src/main/kotlin/lesson_1/ex_5.kt
@@ -7,7 +7,6 @@ fun main() {
 
     val hours = totalSeconds / (TIME_UNIT * TIME_UNIT)
     val secondsAfterHours = totalSeconds % (TIME_UNIT * TIME_UNIT)
-
     val minutes = secondsAfterHours / TIME_UNIT
     val seconds = secondsAfterHours % TIME_UNIT
     println("%02d:%02d:%02d".format(hours, minutes, seconds))
