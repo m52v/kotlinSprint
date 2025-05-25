a8a70dff342fce2ab800d099741616826925b4cb
m52v <morem0t.ev@gmail.com>
2025-05-25 21:06:24 +0700

    KS-3-1 user greeting

---
+++ src/main/kotlin/lesson_3/ex_1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val userName = "Tom"
+    var greeting = "Good afternoon"
+    println("$greeting, $userName!")
+
+    greeting = "Good evening"
+    println("$greeting, $userName!")
+}
