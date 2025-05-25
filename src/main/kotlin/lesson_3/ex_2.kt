99cf23f161d63efcc3773b23a9bf278b6e67032b
m52v <morem0t.ev@gmail.com>
2025-05-25 21:17:03 +0700

    KS-3-2 personal data

---
+++ src/main/kotlin/lesson_3/ex_2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    var lastName = "Андреева"
+    var firstName = "Татьяна"
+    var patronymic = "Сергеевна"
+    var age = 20
+    println("$lastName $firstName $patronymic, $age лет")
+
+    lastName = "Сидорова"
+    age = 22
+    println("$lastName $firstName $patronymic, $age года")
+}
