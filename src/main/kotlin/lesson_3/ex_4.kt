9a61776132b389ae3cb1a5fed3db1f3d758fc9c4
m52v <morem0t.ev@gmail.com>
2025-05-30 06:09:39 +0700

    KS-3-4 chess moves

--- src/main/kotlin/lesson_3/ex_4.kt
+++ src/main/kotlin/lesson_3/ex_4.kt
@@ -5,12 +5,12 @@ fun main() {
     var from = "E2"
     var to = "E4"
     var moveNumber = 1
-    val firstMove = "Ход из $from в $to; номер хода - $moveNumber"
+    val firstMove = "[$from-$to;$moveNumber]"
     println(firstMove)
 
     from = "D2"
     to = "D3"
     moveNumber = 3
-    val secondMove = "Ход из $from в $to; номер хода - $moveNumber"
+    val secondMove = "[$from-$to;$moveNumber]"
     println(secondMove)
 }
